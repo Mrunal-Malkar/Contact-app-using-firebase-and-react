422c4de1a3382c192e7d32a9eb38032bb063bccd
Mrunal is Practising <mrunalpmalkar@gmail.com>
2024-12-27 20:13:29 +0530

    updated vite config

--- contact-app_using-React/vite.config.js
+++ contact-app_using-React/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  baseurl:"/Contact-app-using-firebase-and-react/",
+  base:"/Contact-app-using-firebase-and-react/",
   plugins: [react()],
 })
